46d916455cb1bb765e7de0f0f5bbe745a2f1ed4d
Leonardo Velazquez <leovelazquez812@gmail.com>
2023-08-03 17:23:49 -0600

    feat: changeColor function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ console.log("Patrick is making this page!");
 function clickPatrick() {
   alert("is mayonnaise an instrument?");
 }
+function changeColor(htmlElement, color) {
+//   alert("I'm so excited to change my color!");
+//   console.log(htmlElement);
+//   console.log(color);
+  htmlElement.style.color = color;
+}
